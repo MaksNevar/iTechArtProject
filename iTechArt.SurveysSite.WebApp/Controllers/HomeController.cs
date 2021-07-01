5ba4d667606dba4d048bad2f1990848de2ddf26e
MaksNevar <nevarmaksim20@gmail.com>
2021-07-01 13:01:32 +0300

    Deleted unused staff

--- iTechArt.SurveysSite.WebApp/Controllers/HomeController.cs
+++ iTechArt.SurveysSite.WebApp/Controllers/HomeController.cs
@@ -1,43 +1,12 @@
-﻿using iTechArt.SurveysSite.WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using iTechArt.Common;
-using System.IO;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace iTechArt.SurveysSite.WebApp.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger _logger;
-
-        public HomeController(ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddFile(@"..\iTechArt\Common\logs\HomeControllerLogger.txt");
-            _logger = loggerFactory.CreateLogger("Logger");
-        }
-
-        public string Index()
-        {
-            _logger.LogInformation($"Method saying Hello, User started working at {DateTime.Now:HH:mm:ss}");
-            return "Hello, User";
-        }
-
-        public IActionResult Privacy()
+    {        
+        public IActionResult Index()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        
     }
 }
--- iTechArt.SurveysSite.WebApp/Startup.cs
+++ iTechArt.SurveysSite.WebApp/Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace iTechArt.SurveysSite.WebApp
 {
@@ -27,24 +22,11 @@ namespace iTechArt.SurveysSite.WebApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
