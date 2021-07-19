cea834bdac19ed22caa2251b941b0e9bd3355684
MaksNevar <nevarmaksim20@gmail.com>
2021-07-19 12:00:19 +0300

    Logger finally done

--- src/iTechArt/Logger.cs
+++ src/iTechArt/Logger.cs
@@ -5,17 +5,12 @@ using ILogger = Serilog.ILogger;
 
 namespace iTechArt.Common
 {
-<<<<<<< HEAD
     public sealed class Logger : ILog
-=======
-    public sealed class Logger : IMyLogger
->>>>>>> 575ebb68737b5fc30c9d74a1fb86ecb02121f67b
     {
         private readonly ILogger _logger;
 
         public Logger(ILogger logger) => _logger = logger;
 
-<<<<<<< HEAD
         public void Log(LogLevel level, Exception ex, string message)
         {
             switch (level)
@@ -48,11 +43,6 @@ namespace iTechArt.Common
                 default:
                     throw new ArgumentOutOfRangeException(nameof(level), level, null);
             }
-=======
-        public void Log(string message)
-        {
-            _logger.LogInformation(message);
->>>>>>> 575ebb68737b5fc30c9d74a1fb86ecb02121f67b
         }
     }
 }
