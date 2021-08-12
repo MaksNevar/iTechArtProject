eb9e5ffda70787423f8d41a0b1d2863fb402d68c
MaksNevar <nevarmaksim20@gmail.com>
2021-08-12 10:42:52 +0300

    Using data type attribute in register view model

--- src/iTechArt.SurveysSite.WebApp/ViewModels/RegisterViewModel.cs
+++ src/iTechArt.SurveysSite.WebApp/ViewModels/RegisterViewModel.cs
@@ -7,20 +7,25 @@ namespace iTechArt.SurveysSite.WebApp.ViewModels
         [Required(ErrorMessage = "Please enter login")]
         [StringLength(20, MinimumLength = 5, ErrorMessage = "Login should be from 5 to 20 symbols")]
         [RegularExpression(@"\w*\d*", ErrorMessage = "Login can contain only letters and numbers")]
+        [DataType(DataType.Text)]
         public string Login { get; set; }
 
         [Required(ErrorMessage = "Please enter email")]
         [StringLength(35, MinimumLength = 10, ErrorMessage = "Email should be from 10 to 35 symbols")]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Incorrect email address")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Please enter password")]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "Password should be from 8 to 20 symbols")]
         [RegularExpression(@"\w*\d*", ErrorMessage = "Password can contain only letters and numbers")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [Compare("Password", ErrorMessage = "Passwords are not equal")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password should be from 8 to 20 symbols")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
