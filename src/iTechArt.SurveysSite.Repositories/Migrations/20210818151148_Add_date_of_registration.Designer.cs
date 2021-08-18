85059c49294b65c47f257fb746d14f6f83cd25b3
MaksNevar <nevarmaksim20@gmail.com>
2021-08-18 20:52:06 +0300

    Added date of registration to user model@

--- src/iTechArt.SurveysSite.Repositories/Migrations/20210818151148_Add_date_of_registration.Designer.cs
+++ src/iTechArt.SurveysSite.Repositories/Migrations/20210818151148_Add_date_of_registration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iTechArt.SurveysSite.Repositories.DbContexts;
 
 namespace iTechArt.SurveysSite.Repositories.Migrations
 {
     [DbContext(typeof(SurveysSiteDbContext))]
-    partial class SurveysSiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210818151148_Add_date_of_registration")]
+    partial class Add_date_of_registration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
