93ae3795ae85c0ff6d4521192e6ed26c71dee38c
MaksNevar <79355970+MaksNevar@users.noreply.github.com>
2021-08-20 12:08:48 +0300

    Add three buttons to main page and logo (#9)
    Add three buttons to main page and logo #9

---
+++ src/iTechArt.SurveysSite.WebApp/Controllers/SurveyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTechArt.SurveysSite.WebApp.Controllers
+{
+    [Authorize]
+    public class SurveyController : Controller
+    {
+        public IActionResult MySurveys()
+        {
+            return View();
+        }
+
+        public IActionResult NewSurvey()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/iTechArt.SurveysSite.WebApp/Controllers/SurveysTemplatesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTechArt.SurveysSite.WebApp.Controllers
+{
+    [Authorize]
+    public class SurveysTemplatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
