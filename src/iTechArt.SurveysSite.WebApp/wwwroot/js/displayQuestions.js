620d49b068a5cfd2f4c90cd4f774878879f73675
MaksNevar <nevarmaksim20@gmail.com>
2021-09-17 11:30:24 +0300

    Adding questions with js, html validation

---
+++ src/iTechArt.SurveysSite.WebApp/wwwroot/js/displayQuestions.js
@@ -0,0 +1,21 @@
+﻿function displayQuestion(type, order) {
+    const question = document.querySelector("#question-div");
+    const questionType = question.querySelector("#question-type");
+    const questionTitle = question.querySelector("input.input-question");
+    const questionName = `Questions[${order}]`;
+    questionTitle.setAttribute("name", questionName + ".QuestionTitle");
+    questionType.setAttribute("name", questionName + ".QuestionType");
+    if (type === 1) {
+        const newOptions = document.querySelector("template#closed");
+        const optionsClone = newOptions.content.cloneNode(true);
+        questionType.setAttribute("value", QuestionType.Closed);
+        question.appendChild(optionsClone);
+    }
+    if (type === 2) {
+        const newOptions = document.querySelector("template#openEnded");
+        const optionsClone = newOptions.content.cloneNode(true);
+        questionType.setAttribute("value", QuestionType.OpenEnded);
+        question.appendChild(optionsClone);
+    }
+    question.id = "";
+}
--- src/iTechArt.SurveysSite.WebApp/wwwroot/js/scripts.js
+++ src/iTechArt.SurveysSite.WebApp/wwwroot/js/scripts.js
@@ -1,25 +1,60 @@
-﻿function addQuestion() {
-    var order = document.querySelectorAll(".input-question").length / 2 - 1;
-    var template = document.querySelector("#question");
-    var div = document.querySelector("#questions");
-    var clone = template.cloneNode(true);
-    var questionName = "Questions[" + order + "]";
-    var questionTitle = clone.querySelector("input.input-question");
+﻿class QuestionType {
+    static get Closed() {
+        return 1;
+    }
+
+    static get OpenEnded() {
+        return 2;
+    }
+}
+
+function addQuestion() {
+    const order = document.querySelectorAll("input.input-question").length;
+    const template = document.querySelector("template#question");
+    const div = document.querySelector("#questions");
+    const clone = template.content.cloneNode(true);
+    clone.id = "";
+    const questionName = `Questions[${order}]`;
+    const questionTitle = clone.querySelector("input.input-question");
     questionTitle.setAttribute("name", questionName + ".QuestionTitle");
-    clone.removeAttribute("hidden");
+    const options = clone.querySelector("template#closed");
+    const optionsClone = options.content.cloneNode(true);
+    const questionType = clone.querySelector("#question-type");
+    const questionDiv = clone.querySelector("#question-div");
+    questionType.setAttribute("name", `Questions[${order}].QuestionType`);
+    questionType.setAttribute("value", QuestionType.Closed);
+    questionDiv.appendChild(optionsClone);
     div.appendChild(clone);
 }
 
-function removeQuestion(fieldset) {
-    var div = fieldset.parentElement;
+function removeQuestion(event) {
+    const fieldset = event.target.closest("div.flex");
+    const div = fieldset.parentElement;
     div.removeChild(fieldset);
-    var questionTitle = div.querySelectorAll("input.input-question");
-    for (var i = 0; i < questionTitle.length; i++) {
-        var questionName = "Questions[" + i + "]";
+    const questionTitle = div.querySelectorAll("input.input-question");
+    for (let i = 0; i < questionTitle.length; i++) {
+        const questionName = `Questions[${i}]`;
         questionTitle[i].setAttribute("name", questionName + ".QuestionTitle");
     }
 }
 
-function chooseType(input, type) {
-    input.value = type;
+function chooseType(event, type) {
+    const fieldset = event.target.closest("div.flex");
+    const options = fieldset.querySelectorAll(".question");
+    const currentOption = options[options.length - 1];
+    fieldset.removeChild(currentOption);
+    var newOptions;
+    var optionsClone;
+    const questionType = fieldset.querySelector("#question-type");
+    if (type === QuestionType.Closed) {
+        newOptions = document.querySelector("template#closed");
+        optionsClone = newOptions.content.cloneNode(true);
+        questionType.setAttribute("value", QuestionType.Closed);
+        fieldset.appendChild(optionsClone);
+    } else {
+        newOptions = document.querySelector("template#openEnded");
+        optionsClone = newOptions.content.cloneNode(true);
+        questionType.setAttribute("value", QuestionType.OpenEnded);
+        fieldset.appendChild(optionsClone);
+    }
 }
